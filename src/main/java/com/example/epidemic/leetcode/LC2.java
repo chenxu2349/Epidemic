d90a972932b988b2244fdfbd5ece150ec77398f7
chenxu2349 <chenxu_2349@qq.com>
2023-11-15 11:15:45 +0800

    包目录调整

--- src/main/java/com/example/epidemic/leetcode/LC2.java
+++ src/main/java/com/example/epidemic/leetcode/LC2.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.epidemic.leetcode;
 
 //import java.util.Arrays;
 //import java.util.Scanner;
--- src/main/java/com/example/epidemic/sql/sql1.java
+++ src/main/java/com/example/epidemic/sql/sql1.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.epidemic.sql;
 
 public class sql1 {
     public static void main(String[] args) {
